8cc36b377ed6d3d53670dc6c4985d9f645929a95
Nontoxicon <147211115+Nontoxicon@users.noreply.github.com>
2026-03-17 17:09:41 -0500

    Create wiki-content.js for EarthWars Wiki sections

---
+++ wiki-content.js
@@ -0,0 +1,34 @@
+// EarthWars Wiki Sections
+
+// Home Section
+const homeSection = `
+# Welcome to EarthWars Wiki
+This wiki is your go-to resource for all things EarthWars! Explore guides, strategies, and lore to dominate the game.
+`;
+
+// Quickstart Section
+const quickstartSection = `
+# Quickstart Guide
+
+## Getting Started
+1. Create your account.
+2. Download the game from the official website.
+3. Follow the tutorial to understand the basic mechanics of the game.
+4. Join a community or clan to enhance your gaming experience.
+`;
+
+// Additional Sections
+const additionalSections = `
+# Additional Resources
+- [Official Forums](https://earthwars.com/forums)
+- [Discord Community](https://discord.gg/earthwars)
+- [Wiki Contributions](#)
+`;
+
+const wikiContent = {
+    home: homeSection,
+    quickstart: quickstartSection,
+    additional: additionalSections
+};
+
+module.exports = wikiContent;
